86596dcf64e137eba182a346b55de4949170d4c2
Himanshu Gusain <133968516+ProtoShiva@users.noreply.github.com>
2024-09-29 23:57:26 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,7 @@ import { getAnalytics } from "firebase/analytics"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUEyZaXTbYZyCuhwD5rp1DpEiEIAXQzk0",
-  authDomain: "movies-gpt-708a9.firebaseapp.com",
-  projectId: "movies-gpt-708a9",
-  storageBucket: "movies-gpt-708a9.appspot.com",
-  messagingSenderId: "1002362749974",
-  appId: "1:1002362749974:web:f843992cf9f71ecf4d6137",
-  measurementId: "G-XT3KTTH6N6",
+
 }
 
 const app = initializeApp(firebaseConfig)
